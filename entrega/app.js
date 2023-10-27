2b119b51ebd0faefe85a2eabcfaa0dba7399cda5
Agustin Lopez <127249231+elguantedethanos@users.noreply.github.com>
2023-10-26 21:37:54 -0300

    entrega

---
+++ entrega/app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require('dotenv').config();
+var pool = require('./models/bd');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros');//nosotros.js
+var productosRouter = require('./routes/productos');//productos.js
+var galeriaRouter = require('./routes/galeria');//galeria.js
+var loginRouter = require('./routes/admin/login')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+pool.query('SELECT * FROM empleados').then(function(resultados){
+  //console.log(resultados)
+});
+
+var obj = {
+nombre: 'Juan',
+apellido: 'Lopez',
+trabajo: 'docente',
+edad: 38,
+salario:150000,
+mail: 'juanlopez@gmail.com'};
+
+
+pool.query('insert into empleados set ?', [obj]).then (function (resultados) {
+  console.log(resultados)
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
