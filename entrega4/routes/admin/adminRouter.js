cea2bc4fa105a7cdd0d7c3d7fb6c043170ca513b
Agustin Lopez <127249231+elguantedethanos@users.noreply.github.com>
2023-11-02 22:43:19 -0300

    21123

---
+++ entrega4/routes/admin/adminRouter.js
@@ -0,0 +1,72 @@
+// adminNovedadesRouter.js (Admin Panel Routes for News Articles)
+const express = require('express');
+const router = express.Router();
+var novedadesModel = require('./models/novedadesModel');
+
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
+    res.render('admin/novedades', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+        novedades
+    });
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades');
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo !== "" && req.body.subtitulo !== "" && req.body.cuerpo !== "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargó la novedad'
+        });
+    }
+});
+
+router.get('/modificar/:id', async (req, res, next) =>{
+    let id = req.params.id;
+    let novedad = await novedadesModel.getNovedadById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    });
+});
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        let obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        };
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error);
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modificó la novedad'
+        });
+    }
+});
+
+module.exports = router;
+
