cc179816504d053aa9d82f708ebf86c0c5ef0e1a
Agustin Lopez <127249231+elguantedethanos@users.noreply.github.com>
2024-01-23 11:54:28 -0300

    Update index.js

--- recuperatorio/routes/index.js
+++ recuperatorio/routes/index.js
@@ -2,59 +2,54 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var novedadesModel = require('../models/novedadesModel');
-var novedadesModel = require('../models/novedadesModel');
 
-
-
-router.get('/', async function(req, res, next){
+/* GET home page. */
+router.get('/', async function(req, res, next) {
   var novedades = await novedadesModel.getNovedades();
+  novedades = novedades.splice(0, 5);
   
-  res.render('index',{
+  res.render('index', {
     novedades
   });
 });
 
+/* POST home page. */
 router.post('/', async (req, res, next) => {
-  
-  console.log(req.body);
+  try {
+    console.log(req.body);
 
-  var nombre = req.body.nombre;
-  var apellido = req.body.apellido;
-  var email = req.body.email;
-  var telefono = req.body.tel;
-  var mensaje = req.body.comentarios;
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    var email = req.body.email;
+    var telefono = req.body.tel;
+    var mensaje = req.body.comentarios;
 
-  var obj = {
-    to: 'agustin376@yahoo.com.ar',
-    subject: 'Contacto desde la web',
-    html: nombre + " " + apellido + " se contactó a través y quiere más información a este correo: " + email + ". <br> Además, hizo el siguiente comentario: " + mensaje + ".<br> Su teléfono es " + telefono
-  };
-  // Configuración del transporte SMTP
- 
-  var transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    var obj = {
+      to: 'agustin376@yahoo.com.ar',
+      subject: 'Contacto desde la web',
+      html: nombre + " " + apellido + " se contactó a través y quiere más información a este correo: " + email + ". <br> Además, hizo el siguiente comentario: " + mensaje + ".<br> Su teléfono es " + telefono
+    };
+
+    // Configuración del transporte SMTP
+    var transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+
+    var info = await transporter.sendMail(obj);
+    res.render('index', {
+      message: 'Mensaje enviado correctamente'
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('index', {
+      error: 'Error al enviar el mensaje'
+    });
   }
 });
-var info = await transporter.sendMail(obj);
-res.render('index', { 
-  message:'Mensaje enviado correctamente',
-});
-
-
-});
-
-var novedadesModel = require('../models/novedadesModel');
-/*GET home page.*/
-router.get('/', async function(req, res, next){
-  novedades = await novedadesModel.getNovedades();
-  novedades = novedades.splice(0,5);
-  res.render('index',{
-    novedades
-  });
-}),
 
 module.exports = router;
